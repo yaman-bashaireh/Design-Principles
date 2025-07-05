a887b97277e938cd7a98dbe5e68cc433166cf742
YamanAlbashaireh <yaman.albashairehh@gmail.com>
2025-07-05 19:54:39 +0300

    Add files via upload

---
+++ Encapsulate What Varies/PizzaConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate_What_Varies
+{
+    internal class PizzaConstants
+    {
+        //public readonly static string CheesePizza = "cheese";
+        public const string CheesePizza = "cheese";
+        public const string VegeterianPizza = "veggie";
+        public const string ChickenPizza = "chicken";
+    }
+}
