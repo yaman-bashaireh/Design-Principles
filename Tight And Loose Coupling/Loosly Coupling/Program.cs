639edbdba009b92b27a8e54d310a5674673a9ea5
YamanAlbashaireh <yaman.albashairehh@gmail.com>
2025-07-05 19:56:21 +0300

    Add files via upload

---
+++ Tight And Loose Coupling/Loosly Coupling/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Loosly_Coupling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceModde = NotifacationModeFactory.Create(NotifacationMode.Email);
+            NotificationService notification = new NotificationService(serviceModde);
+            notification.Notify();
+        }
+    }
+
+    enum NotifacationMode
+    {
+        Email,
+        Sms
+    }
+    class NotifacationModeFactory
+    {
+        public static INotificationMode Create(NotifacationMode notifacationMode)
+        {
+            switch (notifacationMode)
+            {
+                case NotifacationMode.Email:
+                    return new EmailService();
+
+                case NotifacationMode.Sms:
+                    return new SmsService();
+
+                default:
+                    return new EmailService();
+            }
+        }
+    }
+
+
+    interface INotificationMode
+    {
+        void Send();
+    }
+    class SmsService : INotificationMode
+    {
+        public void Send()
+        {
+            Console.WriteLine("Sms sent");
+        }
+    }
+    class EmailService : INotificationMode
+    {
+        public void Send()
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+    class NotificationService                                                    // Your task is only to report and not to specify the method, but I will tell you what the method is.
+    {
+        private readonly INotificationMode _notificationMode;
+        public NotificationService(INotificationMode notificationMode)     
+        {
+            _notificationMode = notificationMode;
+        }
+
+        public void Notify()
+        {
+            _notificationMode.Send();
+        }
+
+    }
+}
---
+++ Tight And Loose Coupling/Tightly Coupling/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Tightly_Coupling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationService notificationService = new NotificationService(new EmailService(), new SmsService());
+
+            notificationService.Notify();
+        }
+    }
+
+    class SmsService 
+    {
+        public void Send()
+        {
+            Console.WriteLine("Sms sent");
+        }
+    }
+    class EmailService 
+    {
+        public void Send()
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+
+    class NotificationService
+    {
+        private readonly EmailService _emailService;       // Tightly Coupling 
+        private readonly SmsService _SmsService;           // ...........
+        public NotificationService(EmailService emailService, SmsService smsService)             //May have more services in future, So there is many injection Unfortunately.
+        {
+            _emailService = emailService;
+            _SmsService = smsService;
+        }
+        public void Notify()
+        {
+            _emailService.Send();
+            _SmsService.Send();
+        }
+    }
+}
